0f24038d68f468a4af708dd644cf106b935f504c
neryklin <neryklin@mail.ru>
2024-12-09 16:09:50 +0300

    add test case

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,65 @@ public class Main {
 
     public static void main(String[] args) {
 
+    //test
+        TaskManager taskManager = new TaskManager();
 
+        System.out.println("---------- test task start---------------");
+        Task task = new Task("fist task","paint green button",TaskStatus.NEW);
+        Task task2 = new Task("second  task","paint red button",TaskStatus.NEW);
+        taskManager.addTask(task);
+        taskManager.addTask(task2);
+        System.out.println(taskManager.getTasksList().toString());
+        task2.setName("new fist task");
+        task2.setStatus(TaskStatus.IN_PROGRESS);
+        taskManager.updateTask(task2);
+        System.out.println(taskManager.getTasksList().toString());
+        System.out.println("---------- remove test task --------------");
+        taskManager.removeTaskFromId(task2.getId());
+        System.out.println(taskManager.getTasksList().toString());
+        System.out.println("---------- test task end ---------------");
+
+        System.out.println("---------- test epic start---------------");
+        Epic epic = new Epic("fist epic","epic green button",TaskStatus.NEW);
+        Subtask subtask = new Subtask("subtask 1","open the color",TaskStatus.NEW,epic);
+        Subtask subtask2 = new Subtask("subtask 2","get brush",TaskStatus.NEW,epic);
+        Subtask subtask3 = new Subtask("subtask 3","paint the button",TaskStatus.NEW,epic);
+        Epic epic2 = new Epic("second  epic","epic red button",TaskStatus.NEW);
+        Epic epic3 = new Epic("third  epic","third",TaskStatus.NEW);
+        Subtask subtask4 = new Subtask("subtask 4","TEST 2",TaskStatus.NEW);
+        System.out.println("---------- list epic--------------");
+
+        taskManager.addEpicSubTask(epic3,subtask4);
+
+        taskManager.addEpic(epic);
+        taskManager.addEpic(epic2);
+        taskManager.addEpic(epic3);
+
+        System.out.println(taskManager.getEpicsList().toString());
+        System.out.println("---------- remove epic ---------------");
+        taskManager.removeTaskFromId(epic3.getId());
+        System.out.println(taskManager.getEpicsList().toString());
+        System.out.println("---------- test epic end---------------");
+
+
+
+
+////        System.out.println(taskManager.getTasksList().toString());
+////        System.out.println("-----------------------------------");
+//        System.out.println(taskManager.getEpicsList().toString());
+////        System.out.println("-----------------------------------");
+//        System.out.println(taskManager.getSubTasksListFromEpic(epic).toString());
+//       //  Subtask subtask22 = new Subtask("subtask update","open the color",TaskStatus.DONE,epic);
+//        Task subtask22 = taskManager.getTaskFromId(3);
+//        subtask22.setStatus(TaskStatus.DONE);
+//        taskManager.addEpicSubTask(epic,(Subtask) subtask22);
+//        System.out.println("-----------------------------------");
+//        System.out.println(taskManager.getSubTasksListFromEpic(epic).toString());
+////        System.out.println("++++++++++++");
+//        System.out.println(taskManager.getEpicsList().toString());
+////        taskManager.romoveTaskFromId(subtask22.getId());
+////        System.out.println("//////");
+////        System.out.println(taskManager.getEpicsList().toString());
 
     }
 }
